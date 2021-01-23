4880f85378b863a4f29744c4d16afa7e4d511fb4
Miner3210 <patsakorn.mark@mail.kmutt.ac.th>
2021-01-23 15:42:01 +0700

    make it better

--- src/app_01_home.py
+++ src/app_01_home.py
@@ -21,14 +21,14 @@ def webhook():
     print('here num1 = {0}'.format(num1))
     print('here num2 = {0}'.format(num2))
     fulfillmentText = 'The sum of the two numbers is '+sum
-  elif query_result.get('action') == 'multiply.numbers':
+  if query_result.get('action') == 'multiply.numbers':
     num1 = int(query_result.get('parameters').get('number'))
     num2 = int(query_result.get('parameters').get('number1'))
     product = str(num1 * num2)
     print('here num1 = {0}'.format(num1))
     print('here num2 = {0}'.format(num2))
     fulfillmentText = 'The product of the two numbers is '+product
-    elif query_result.get('action') == 'Listmenu':
+    if query_result.get('action') == 'Listmenu':
        with open('food.json') as json_file:
         data = json.load(json_file)
         s = ""
