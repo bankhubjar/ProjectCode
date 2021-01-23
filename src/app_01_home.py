bb7fd975b03abf7f3933468ea5ee97549fa8ba33
Miner3210 <patsakorn.mark@mail.kmutt.ac.th>
2021-01-23 15:39:03 +0700

    make it better

--- src/app_01_home.py
+++ src/app_01_home.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import Flask,request
+import json
 appBlueprint = Blueprint("home",__name__)
 
 @appBlueprint.route("/")
@@ -7,18 +8,40 @@ def index():
     return 'Hello, I am Flask Application ssss.'
 
 
-@appBlueprint.route('/webhook',methods=['POST'])
+@appBlueprint.route('/webhook', methods=['POST'])
 def webhook():
   req = request.get_json(silent=True, force=True)
+  fulfillmentText = ''
   sum = 0
   query_result = req.get('queryResult')
-  num1 = int(query_result.get('parameters').get('number'))
-  num2 = int(query_result.get('parameters').get('number1'))
-  sum = str(num1 + num2)
-  print('here num1 = {0}'.format(num1))
-  print('here num2 = {0}'.format(num2))
-  return {
-        "fulfillmentText": 'The sum of the two numbers is: the fuck dude'+sum,
+  if query_result.get('action') == 'add.numbers':
+    num1 = int(query_result.get('parameters').get('number'))
+    num2 = int(query_result.get('parameters').get('number1'))
+    sum = str(num1 + num2)
+    print('here num1 = {0}'.format(num1))
+    print('here num2 = {0}'.format(num2))
+    fulfillmentText = 'The sum of the two numbers is '+sum
+  elif query_result.get('action') == 'multiply.numbers':
+    num1 = int(query_result.get('parameters').get('number'))
+    num2 = int(query_result.get('parameters').get('number1'))
+    product = str(num1 * num2)
+    print('here num1 = {0}'.format(num1))
+    print('here num2 = {0}'.format(num2))
+    fulfillmentText = 'The product of the two numbers is '+product
+    elif query_result.get('action') == 'Listmenu':
+       with open('food.json') as json_file:
+        data = json.load(json_file)
+        s = ""
+        for p in data['eng_name']:
+            s += ","+p
+        fulfillmentText = 'list menu have  '+s
+       return {
+        "fulfillmentText": fulfillmentText,
         "displayText": '25',
         "source": "webhookdata"
     }
+
+
+
+
+        
