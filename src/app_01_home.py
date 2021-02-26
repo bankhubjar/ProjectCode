6358ceec01a143f10fdd9516872c214b141f7c92
Miner3210 <patsakorn.mark@mail.kmutt.ac.th>
2021-02-26 11:42:43 +0700

    [ADD] Google calendar API

--- src/app_01_home.py
+++ src/app_01_home.py
@@ -1,16 +1,33 @@
+#Python Flask
 from flask import Blueprint
 from flask import Flask, render_template
 from flask import Flask,request
-from firebase import firebase
+from flask import Flask, render_template, url_for, json
+
 import urllib
 from urllib.parse import unquote
+
+#FirebaseDatabase
 import firebase_admin
+from firebase import firebase
 from firebase_admin import credentials
 from firebase_admin import db
 import os
+
+#Time
 from datetime import datetime
 import pytz
-from flask import Flask, render_template, url_for, json
+
+# Googlecalendar Api
+import os.path
+
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
 tz = pytz.timezone('Asia/Bangkok')
 
 # Fetch the service account key JSON file contents
@@ -28,6 +45,45 @@ ref = db.reference('/users')
 firebase = firebase.FirebaseApplication('https://testproject-9cef3-default-rtdb.firebaseio.com/', None)
 import json
 appBlueprint = Blueprint("home",__name__)
+
+@appBlueprint.route('/calendar')
+def calendar():
+    
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                './src/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    start = ''
+    if not events:
+        start ='No upcoming events found.'
+    for event in events:
+        start = start +event['start'].get('dateTime', event['start'].get('date'))+''+event['summary']+"<br>"
+    return start   
+
 
 
 @appBlueprint.route('/webhook',methods=['POST'])
