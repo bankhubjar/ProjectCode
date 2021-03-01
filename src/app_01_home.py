6b3cd2ac1990986da45c4817c7ca66c6c9edc4db
Miner3210 <patsakorn.mark@mail.kmutt.ac.th>
2021-03-01 19:10:49 +0700

    [UPDATE] some problem

--- src/app_01_home.py
+++ src/app_01_home.py
@@ -615,7 +615,7 @@ def rejectOrder():
         startdateformat = str(todaydate).split(" ")[0]+"T"+str(todaydate).split(" ")[1].split(".")[0]+"+07:00"
         newcheck = str(enddaydate).split(" ")[0]+"T"+str(enddaydate).split(" ")[1]+"+07:00"
         return {
-        "fulfillmentText": testcheck(todaydate,newcheck),
+        "fulfillmentText": testcheck(startdateformat,newcheck),
         "displayText": '50',
         "source": "webhookdata"
       }
